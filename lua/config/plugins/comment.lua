f1b8d58224023e7e506b4a859f27afe4f1a46231
Isro Hidayatulloh <orsi.amax@gmail.com>
2026-06-13 00:11:27 +0700

    Fix: error while using gc to commenting
    there is error to commenting, need to remove some package and make
    manual commenting work using nvim/vim default keymap.

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,36 +1,24 @@
 return {
 	{
 		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = true,
+		lazy = false,
 		opts = {
 			enable_autocmd = false, -- Let Comment.nvim handle triggering
 		},
-	},
-	{
-		"numToStr/Comment.nvim",
-		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-		keys = {
-			{ "gc", mode = { "n", "v" } },
-			{ "gb", mode = { "n", "v" } },
-			{ "gcc", mode = "n" },
-			{ "gbc", mode = "n" },
-		},
-		opts = function()
-			return {
-				padding = true,
-				sticky = true,
-				ignore = "^$",
-				pre_hook = function(ctx) -- added fall back to vim.bo.commentstring is pre hook returns nil
-					local ts_pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-					local commentstring = ts_pre_hook(ctx)
+        config = function (_, opts)
+            require("ts_context_commentstring").setup(opts)
 
-					if not commentstring then
-						return vim.bo.commentstring or "# %s"
-					end
-
-					return commentstring
-				end,
-			}
-		end,
+            -- Integrasikan deteksi konteks langsung ke dalam sistem komentar bawaan Neovim
+            local get_option = vim.filetype.get_option
+            vim.filetype.get_option = function (filetype, option)
+                if option == "commentstring" then
+                    local commentstring = require("ts_context_commentstring.internal").calculate_commentstring()
+                    if commentstring then
+                        return commentstring
+                    end
+                end
+                return get_option(filetype, option)
+            end
+        end
 	},
 }
