fb75300c05a1dae6c89bbea663e9017cc745bf4a
Isro Hidayatulloh <orsi.amax@gmail.com>
2026-06-13 00:09:38 +0700

    Feat: Adding user command
    Create custom command to create new project and remove comments on new
    main.dart

--- lua/config/plugins/flutter.lua
+++ lua/config/plugins/flutter.lua
@@ -44,34 +44,90 @@ return {
         -- ===================================================================================== 
         -- USER COMMAND FOR RUNNING FLUTTER BASH SCRIPT
         -- =====================================================================================
-        vim.api.nvim_create_user_command("FlutterNew", function (opts)
+        vim.api.nvim_create_user_command("FlutterNew", function(opts)
             local project_name = opts.args
 
             if project_name == "" then
-                vim.notify("Error: Project name cannot be empty!", vim.log.levels.ERROR)
+                vim.notify("Error: Project name can't be empty!", vim.log.levels.ERROR)
                 return
             end
 
-            -- Running Bash Script asynchronously to prevent freeze
-            vim.fn.jobstart({ "/home/" .. vim.uv.os_getenv("USER") .. "/scripts/flutter-new.sh", project_name  }, {
+            local clean_name = string.lower(project_name):gsub("-", "_")
+            -- Getting absolute path of current active directory
+            local current_dir = vim.fn.getcwd()
+            local full_project_path = current_dir .. "/" .. clean_name
+
+            vim.notify(full_project_path)
+            -- vim.notify("Executing script...", vim.log.levels.INFO)
+
+            local error_output = {}
+
+            -- Running bash script asynchronously
+            vim.fn.jobstart({ "/home/" .. vim.uv.os_getenv("USER") .. "/scripts/flutter-new.sh", project_name }, {
                 stdout_buffered = true,
                 stderr_buffered = true,
-                on_exit = function (_, exit_code, _)
+                on_exit = function(_, exit_code, _)
                     if exit_code == 0 then
-                        -- Karena di dalam Bash Script ada perintah 'cd', kita sinkronisasikan
-                        -- Neovim agar otomatis ikut masuk ke folder project baru tersebut.
-                        local clean_name = string.lower(project_name):gsub("-", "_")
-                        vim.cmd("cd " .. clean_name)
-                        vim.cmd("edit lib/main.dart")
-                        vim.notify("Project sukses dibuat dan dimuat!", vim.log.levels.INFO)
+                        -- 1. Move Neovim working directory to new project
+                        -- vim.cmd("cd " .. full_project_path)
+
+                        -- 2. Waiting 100ms to let Neovim asynchronousing new folder before open dart file
+                        -- vim.defer_fn(function()
+
+                        vim.schedule(function()
+                            vim.cmd("cd " .. full_project_path)
+                            -- Open file main.dart using absolute path
+                            local main_dart_path = full_project_path .. "/lib/main.dart"
+                            vim.cmd("edit " .. main_dart_path)
+
+                            -- Ask oil.nvim to refreshing
+                            pcall(function() require("oil").refresh() end)
+
+                            vim.notify("Project successfully created! Happy coding.", vim.log.levels.INFO)
+                        end)
                     else
-                        vim.notify("Gagal mengeksekusi script flutter-new.sh", vim.log.levels.ERROR)
+                        local err_msg = table.concat(error_output, "\n")
+                        if err_msg == "" then err_msg = "Unknown error (Exit code: " .. exit_code .. ")" end
+
+                        vim.notify("Gagal \n" .. err_msg, vim.log.levels.ERROR)
                     end
                 end,
-            })
-        end,{
+            }) -- end vim.fn.jobstart()
+        end, {
                 nargs = 1,
-                desc = "Membuat Project Flutter baru dengan menjalankan perintah bash script",
+                desc = "Create Flutter project using external command inside nvim."
+            })
+        -- ===================================================================================== 
+        -- end of user command
+        -- =====================================================================================
+
+        -- ===================================================================================== 
+        -- USER COMMAND TO REMOVE ALL COMMENTS ON FLUTTER TEMPLATE
+        -- =====================================================================================
+        vim.api.nvim_create_user_command("FlutterCleanComments", function()
+            -- 1. Save cursor current position in order not jump while clean up
+            local save_cursor = vim.fn.getpos(".")
+
+            -- 2. Run Regex subtitution on all file (%)
+            -- using pcall (protected call) in order no error appears while everything is clean
+            pcall(function()
+                -- Menghapus semua baris murni (// ...) termasuk spasi di depannya
+                vim.cmd([[%s/\s*\/\/.*$//ge]])
+
+                -- Menghapus baris-baris kosong beruntun yang ditingalkan oleh bekas komentar
+                -- agar struktur kode menjadi rapi
+                vim.cmd([[g/^\s*$/d]])
+            end)
+
+            -- 3. Kembalikan posisi kursor ke tempat semula
+            vim.fn.setpos(".", save_cursor)
+
+            -- 4. Jalankan auto-format LSP (jika aktif) agar indentation kembali rapi
+            vim.lsp.buf.format({ async = true })
+
+            vim.notify("Komentar berhasil dihapus.", vim.log.levels.INFO)
+        end, {
+                desc = "Erase all comment on main.dart",
             })
 
         -- Custom Keymaps untuk kebutuhan Flutter workflow
